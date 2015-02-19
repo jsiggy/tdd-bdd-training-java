99f611624e554d6bd61093c90a91180924bb6151
John Sigler <john_sigler@intuit.com>
2015-02-19 11:16:15 -0600

    Verify adding 3 integers works

--- src/test/java/training/tdd/FractionTest.java
+++ src/test/java/training/tdd/FractionTest.java
@@ -40,4 +40,13 @@ public class FractionTest {
 
       assertEquals(expected, result);
    }
+
+   @Test
+   public void canAddMoreThanTwoIntegers() {
+      Fraction expected = new Fraction(6);
+
+      Fraction result = new Fraction(1).plus(new Fraction(2)).plus(new Fraction(3));
+
+      assertEquals(expected, result);
+   }
 }
