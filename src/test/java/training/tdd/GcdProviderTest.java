ebdfbaa79ff49d27b523f60c74298e20d1b5749d
John Sigler <john_sigler@intuit.com>
2015-02-19 16:16:10 -0600

    Contract Test example where we pull our tests up to abstract test

---
+++ src/test/java/training/tdd/GcdProviderTest.java
@@ -0,0 +1,36 @@
+package training.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public abstract class GcdProviderTest {
+   @Test
+   public void canCalculateGcd() {
+      int expected = 2;
+
+      int actual = createGcdProvider().gcd(6, 4);
+
+      assertEquals(expected, actual);
+   }
+
+   @Test
+   public void shouldAlwaysReturnAPositiveResult() {
+      int expected = 5;
+
+      int actual = createGcdProvider().gcd(-10, -25);
+
+      assertEquals(expected, actual);
+   }
+
+   @Test
+   public void shouldReturnAbsoluteValueOfOriginalNumberForGcdOfAnythingAndZero() {
+      int expected = 7;
+
+      int actual = createGcdProvider().gcd(-7, 0);
+
+      assertEquals(expected, actual);
+   }
+
+   protected abstract SimpleGcdProvider createGcdProvider();
+}
--- src/test/java/training/tdd/SimpleGcdProviderTest.java
+++ src/test/java/training/tdd/SimpleGcdProviderTest.java
@@ -1,38 +1,8 @@
 package training.tdd;
 
-import org.junit.Test;
+public class SimpleGcdProviderTest extends GcdProviderTest {
 
-import static org.junit.Assert.*;
-
-public class SimpleGcdProviderTest {
-   @Test
-   public void canCalculateGcd() {
-      int expected = 2;
-
-      int actual = createGcdProvider().gcd(6, 4);
-
-      assertEquals(expected, actual);
-   }
-
-   @Test
-   public void shouldAlwaysReturnAPositiveResult() {
-      int expected = 5;
-
-      int actual = createGcdProvider().gcd(-10, -25);
-
-      assertEquals(expected, actual);
-   }
-
-   @Test
-   public void shouldReturnAbsoluteValueOfOriginalNumberForGcdOfAnythingAndZero() {
-      int expected = 7;
-
-      int actual = createGcdProvider().gcd(-7, 0);
-
-      assertEquals(expected, actual);
-   }
-
-   private SimpleGcdProvider createGcdProvider() {
+   @Override protected SimpleGcdProvider createGcdProvider() {
       return new SimpleGcdProvider();
    }
 }
