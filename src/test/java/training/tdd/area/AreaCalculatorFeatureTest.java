a1769c4413a32af7871cee65104667d37d20aa89
John Sigler <john_sigler@intuit.com>
2015-02-21 14:02:45 -0600

    Add several "sanity" tests to verify things are working when integrated... looking for anything that uses a collaborator such as our idProvider

--- src/test/java/training/tdd/area/AreaCalculatorFeatureTest.java
+++ src/test/java/training/tdd/area/AreaCalculatorFeatureTest.java
@@ -1,6 +1,7 @@
 package training.tdd.area;
 
 import org.junit.Test;
+import training.tdd.area.shapes.Circle;
 import training.tdd.area.shapes.Square;
 
 import static org.junit.Assert.*;
@@ -18,5 +19,35 @@ public class AreaCalculatorFeatureTest {
       double area = board.areaOfShapes();
 
       assertEquals(13.0, area, FUDGE_DELTA);
+   }
+
+   @Test
+   public void canAddDifferentTypesOfShapes() {
+      Board board = new Board().add(new Square(3)).add(new Circle(5));
+
+      int count = board.shapeCount();
+
+      assertEquals(2, count);
+   }
+
+   @Test
+   public void canComputerAreaForTypesOfShapes() {
+      Board board = new Board().add(new Square(10)).add(new Circle(2));
+
+      double area = board.areaOfShapes();
+
+      assertEquals(112.56637, area, FUDGE_DELTA);
+   }
+
+   @Test
+   public void canRemoveAnObjectById() {
+
+      Circle circleToRemove = new Circle(4);
+      Board board = new Board().add(new Circle(10)).add(new Square(2)).add(circleToRemove).add(new Circle((5)));
+
+      board.remove(circleToRemove);
+
+      assertEquals(3, board.shapeCount());
+      assertFalse(board.contains(circleToRemove));
    }
 }
