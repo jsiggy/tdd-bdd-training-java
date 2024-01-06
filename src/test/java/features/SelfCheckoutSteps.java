300c8284b2b3e0e512f1e64678a78423abb15677
john.sigler <john.sigler@gmail.com>
2024-01-06 17:34:35 -0600

    Finally updated! New lib versions and added Cucumber.

--- src/test/java/features/SelfCheckoutSteps.java
+++ src/test/java/features/SelfCheckoutSteps.java
@@ -1,31 +1,20 @@
 package features;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import training.checkout.Product;
-import training.checkout.ProductCatalog;
-import training.checkout.Scanner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelfCheckoutSteps {
-    private final ProductCatalog productCatalog = new ProductCatalog();
-    private Product product;
-
-    @Given("the product catalog includes a {word} with barcode {long} and price ${double}")
-    public void addProductToCatalog(String productName, Long barcode, Double price) {
-        productCatalog.add(new Product(barcode, productName, price));
+    @Given("the product catalog includes a lightbulb with barcode {int} and price ${double}")
+    public void the_product_catalog_includes_a_lightbulb_with_barcode_and_price_$(Integer int1, Double double1) {
     }
 
-    @When("a product with barcode {long} is scanned")
-    public void scanProductByBarcode(Long barcode) {
-        product = new Scanner(productCatalog).scan(barcode);
+    @When("a product with barcode {int} is scanned")
+    public void a_product_with_barcode_is_scanned(Integer int1) {
     }
 
     @Then("the current product price should be ${double}")
-    public void productPriceShouldBe(Double expectedPrice) {
-        assertEquals(expectedPrice, product.getPrice());
+    public void the_current_product_price_should_be_$(Double double1) {
     }
 }
-
